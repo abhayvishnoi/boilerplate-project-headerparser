c764c86539dcc797a2528b2072367a6f0047943f
Abhay VIshnoi <abhayvishnoi6@gmail.com>
2024-10-06 19:46:09 +0000

    add

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  let resObject={
+    ipaddress:req.ip,
+    language:req.headers['accept-language'],
+    software:req.headers['user-agent']
+  }
+  res.json(resObject)
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
